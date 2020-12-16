6aa96d92110a915f01a6af8eee16d743efc3a1f7
Ciro Pompeu <ciro.pompeu@bol.com.br>
2020-12-16 10:35:43 -0300

    ainda faltam os exercícios

---
+++ revisao/destructuring.js
@@ -0,0 +1,30 @@
+// destructuring
+// desestruturação ou alocação por desestruturação
+
+// não modifica nem altera o array de origem
+
+// extrair dados de array ou objetos literais
+
+
+
+
+let pessoa = {
+    nome: "vinicius",
+    altura: 1.80,
+}
+
+// uma opcao seria let nome = pessoa.nome;
+
+const {nome, altura} = pessoa;
+
+console.log(nome);
+console.log(altura);
+
+let listaCompras = ["pão", "leite", "açúcar"];
+
+const [item1, item2, item3] = listaCompras;
+
+console.log(item1);
+console.log(item2);
+console.log(item3);
+
