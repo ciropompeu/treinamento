f9d1953d39b2b1fcddf687315773f3a02abe649f
Ciro Pompeu <ciro.pompeu@bol.com.br>
2020-12-22 21:41:14 -0300

    callback com exercicios

--- json/callback.js
+++ json/callback.js
@@ -1,14 +1,103 @@
-// callback
-// passar uma função como parâmetro
+//callback funcao que passa como parametro outra funcao
 
-// métodos map, filter, reduce 
+const somar = (a, b) => a + b;
+const calculadora = (a, b, operacao) => operacao(a, b);
 
-// um calll bacj é uma função que se passa como parâmetro para outra função.
-// a função que recebe é que se encarrega de executá-lo quando seja necessário
 
-setTimeout (function(){
-    console.log('Hola Mundo!');
-}, 1000)
+// chamando a funcao como parametro
+console.log(calculadora(10, 20, somar));
 
-let meuCallback = () => console.log('ólá mundo');
-setTimeout(meuCallback, 1000);
+// criando uma funcao dentro do parametro
+console.log(calculadora(12, 24, (a,b) => a - b));
+
+//exercicio dobro e triplo
+
+const dobro = a => a*2;
+const triplo = a => a*3;
+const aplicar = (a,operacao) => operacao(a);
+
+console.log(aplicar(12, triplo));
+
+/*
+
+jeito comprido
+function dobro(a) {
+     return a*2;
+}
+function triplo(a) {
+     return a*3;
+}
+function aplicar (a,operacao) {
+    return operacao(a);
+}
+*/
+
+// calculadora
+
+const somar = (a,b) => a+b;
+const subtrair = (a,b) => a-b;
+const multiplicar = (a,b) => a*b;
+const dividir = (a,b) => a/b;
+
+const calculadora = (a,b,operacao) => operacao(a,b);
+
+console.log(calculadora(121,11,dividir));
+
+/* jeito comprido
+
+function somar(a,b){
+    return a+b;
+}
+function subtrair(a,b){
+    return a-b;
+}
+function multiplicar(a,b){
+    return a*b;
+}
+function dividir(a,b){
+    return a/b;
+}
+
+function calculadora(a,b,operacao){
+    return operacao(a,b);
+}
+
+console.log(calculadora(121,11,dividir));
+
+*/
+
+// passando um callback
+
+let valores = ["www.terra.com.br","www.google.com.br"];
+
+
+const adicionarHttp = (valor) => "http://" + valor;
+const processar = (lista, operacao) => {
+    for (let i = 0; i < lista.length; i++) {
+        lista[i] = operacao(lista[i]);
+    }
+    return lista;
+}
+
+console.log(processar(valores, adicionarHttp));
+
+
+/*
+
+comprido
+
+let colecao = ["1","2","3"];
+
+function adicionarHttp(url) {
+    return "http://" + url;
+}
+
+function processar(lista, operacao) {
+ for (let i = 0; i < lista.length; i++){
+     lista[i] = operacao(lista[i]);
+ }
+ return lista;
+}
+
+console.log(processar(colecao, adicionarHttp));
+ */
