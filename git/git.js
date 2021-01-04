5cb818b18d87c531031adcc6f05f84ccb34c93f5
Ciro Pompeu <ciro.pompeu@bol.com.br>
2021-01-04 16:07:01 -0300

    instruções ao repositorio git

---
+++ git/git.js
@@ -0,0 +1,23 @@
+// introducao ao git - software de contro de versões
+// backup completo dos arquivos e compartilhar os mesmos com a equipe de colaboradores
+
+
+// comandos
+
+git init // cria o repositório
+git config user.name "nomeUsuario" // define o usuário
+git config user.email "emailUsuario" // define o email
+git remote add origin http://...   // aponta o repositório como remoto
+git add . // adiciona todas as modificações
+
+git commit -m "mensagem" // comita(salva) as modificações feitas
+git push origin master // envia as modificações ao repositório remoto
+
+/*  documentação
+
+dev.to/t/git
+oshitgit.com
+git-scm.com
+
+*/
+
