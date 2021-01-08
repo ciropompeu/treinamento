86554cebc70d4c99d68927f7a4dba9d22cea5993
Ciro Pompeu <ciro.pompeu@bol.com.br>
2021-01-07 22:20:26 -0300

    enunciado exercicio 2

--- revisao/revisao-exercicios-aula201210.js
+++ revisao/revisao-exercicios-aula201210.js
@@ -1,5 +1,10 @@
-const valores = [1, 2, 5, 23, 6, 6, 53, 34, 6, 4];
+/*
 
+//exercicio 1 - enunciado
+//usar um array contendo números aleatórios, realizar um for/of multiplicando o valor por 3 e printar no console
+//plus: multiplicar somente os números pares
+
+const valores = [1, 2, 5, 23, 6, 6, 53, 34, 6, 4];
 
 for (const numeros of valores) {
     console.log(numeros * 3)
@@ -11,4 +16,10 @@ for (const numeros of valores) {
         console.log(numeros * 3)
     }
 }
+
+// exercício 2 - enunciado:
+// Criar um array com  objetos que contenham nome e data de nascimento, após criar uma arrow function que faça primeiramente a desestruturação do array pela pessoa2 e depois desestruture o objeto para capturar o nome da pessoa.. Imprimir o nome da pessoa no console.
+
+*/
+
 
