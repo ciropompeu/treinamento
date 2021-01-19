670b9480ad67ead51a2496e1d30694556d916834
Ciro Pompeu <ciro.pompeu@bol.com.br>
2021-01-18 23:24:27 -0300

    aula 01 / arquitetura cliente/servidor

---
+++ revisao/node/node.js
@@ -0,0 +1,26 @@
+console.log ("aula 01 - arquitetura cliente/servidor");
+
+// arquitetura client-side
+// cliente é o dispositivo que faz solicitações de determinados serviços e recursos para um servidor
+// esse dispositivo pode ser de qualquer tipo, computador, tablet, televisão, etc
+
+// nós como clientes, acessamos um serviço de um servidor através de uma conexão da internet, utilizando um navegador chamado de browser
+
+// request e response (pedido e resposta)
+// quando acessamos um site estamos enviando um request para o servidor e o mesmo, analisa e manda um response
+
+// no servidor tem um webserver, que é o programa encarregado de processar as solicitações feitas pelos clientes
+// gerar e enviar as respostas
+
+// front-end - tudo o que o usuário ve
+// back-end - tudo o que acontece dentro do servidor
+
+// front-end - utiliza html, css, javascript
+// back-end - utiliza php, java, c#, sql (informações dentro de um banco de dados), JSON e XML (para lidar com informações dentro de arquivos)
+
+// com os avanços tecnologia, atualmente é possível executar javascript no lado do servidor
+// essa é uma façada da plataforma chamada node.js
+
+
+
+
