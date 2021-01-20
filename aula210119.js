d04de5c329b447ae1f58aac0536c464a20e041fb
Ciro Pompeu <ciro.pompeu@bol.com.br>
2021-01-20 20:13:38 -0300

    aula 05 / metodologias ágeis

---
+++ aula210119.js
@@ -0,0 +1,117 @@
+console.log ("aula 05");
+console.log("Metodologias ágeis");
+console.log("professor José Carlos Adão");
+
+/* 
+
+Metodologias ágeis
+
+
+Frameowrk 
+SCRUM
+
+Product owner
+ - Ter a ciência de como as tarefas são implementadas
+ - Liderança sobre o produto
+ - Decide quais recursos devem ser construídos e sua ordem
+ - Comunica o objetivo para o grupo
+
+Scrum master
+ - Auxiliar no envolvimento das práticas
+ - Principal conhecedor
+ - Liderança do projeto
+ - Facilitador e não chefe
+
+Nem o product owner nem o scrum master é chefe da equipe, nem líder da equipe
+O time, a equipe é auto-gerenciável
+
+Dev team
+- construtores do projeto
+- tomam as decisões mais importantes
+- auto organização da equipe
+- atingir a meta do product owner
+- quem vai de fato executar as tarefas
+
+
+Como funciona a dinãmica do scrum
+1. visão do produto
+ - documento que descreve o que queremos e onde queremos chegar
+ - descrever a lista de funcionalidades
+ - ordenar por grau de importância
+ - projeto organizado em períodos
+
+Artefatos do product backlog
+são conjunto de atividades que serão colocados em prioridades para execução em cada sprint
+
+Sprint
+trecho de etadas, dentro da etapas de prioridades
+
+sprint backlog é um trecho de atividades do product backlog para ser realizado na spring
+sprint é um trecho de atividade que o dev team vai atuar diretamente
+time boxed (tempo fixo) - contabilizado entre 2 a 4 semanas
+
+sprint planning
+ - ocorre antes do sprint começar
+ - criação do backlog
+ - definição das funcionalidades x tempo da sprint
+
+- seleção das funcionalidades do sprint 1  (imprescindível)
+- entrega de uma etapa
+- funcionalidades da sprint 2 (importante)
+- as mudanças que surgirem devem ser inseridas no backlog
+- repetição até entrega do produto final
+
+reuniões diárias
+
+> o que fe ontem?
+> o que vai fazer hoje?
+> tem algum impedimento?
+
+ - por 15 minutos
+ - as perguntas devem ser respondidas
+ - foco no sprint e sua meta
+ - as respostas mostram o avanço do tim
+
+ burndown chart
+
+  - usado em 90% dos projetos
+  - exemplo
+  - relaciona itens a fazer x tempo
+  - projeto ideal x real
+  - adiantado x atrasado
+
+retrospectiva
+ - o que foi positivo
+ - o que foi negativo
+ - o que deve ser melhorado
+ - o que deixará de ser feito
+
+
+Framework
+Kanban (deixar a vista o que precisa ser feito)
+
+painel fzer / fazendo / feito
+
+ - gestão à vista e transparência
+ - visualize o fluxo de trabalho
+ - post-its representam as funcionalidades do backlog (product backlog)
+
+
+scrum
+backlog do projeto BAR
+
+funcionalidades:
+ - cadastrar clientes
+ - cadastrar bebidas
+ - cadastrar pratos
+
+sentar com o devteam e conversar sobre cada funcionalidade e dividir em sprint
+ - sprint 1:    cadastrar clientes
+ - sprint 2:    cadastrar pratos
+                cadastrar bebidas
+
+
+
+
+                
+  */
